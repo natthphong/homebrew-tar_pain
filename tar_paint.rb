564447c815b3d3646a097db8cb6c80bad3604da1
natthaphong <tazamaak07@gmail.com>
2024-07-23 17:36:00 +0700

    add

--- tar_paint.rb
+++ tar_paint.rb
@@ -6,12 +6,11 @@ class TarPaint < Formula
   url "https://files.pythonhosted.org/packages/d5/61/3d77e5c85ae270779d333c94178a7fe549def45b03ea18b08eed6868302a/tar_paint-0.1.0.tar.gz"
   sha256 "5571b88d5e27f388ef88cf898fc346726b141fadab137bd28ee5c97cd4e753d0"
 
-  depends_on "python@3.9"
+  depends_on "python@3.8"
 
 
   def install
     virtualenv_install_with_resources
-    system "pip3", "install", "tk"
     bin.install_symlink libexec/"bin/tar_paint"
   end
 
