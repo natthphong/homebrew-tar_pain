69cde2e1e86f451e8bede78e009a409a3445cfdb
natthaphong <tazamaak07@gmail.com>
2024-07-23 17:49:32 +0700

    revert

--- tar_paint.rb
+++ tar_paint.rb
@@ -12,7 +12,6 @@ class TarPaint < Formula
   def install
     virtualenv_install_with_resources
     bin.install_symlink libexec/"bin/tar_paint"
-    (prefix/"Applications").install libexec/"bin/tar_paint"
   end
 
   test do
